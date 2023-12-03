3212bb562c90b4507cb99d153c5c5ecc90fbbd1d
nambatu <julianlangschwert@gmail.com>
2023-12-03 14:45:17 +0100

    Davids R files added and directories fixed

---
+++ data_cleaning.R
@@ -0,0 +1,67 @@
+rm(list = ls())
+
+library(tidyverse)
+library(zoo)
+
+train_target <- read_csv("dc_proj/Y_train_sl9m6Jh.csv")
+
+train_data <- read_csv("dc_proj/X_train_v2.csv") %>%
+  mutate(
+    Time = dmy_hm(Time)
+  ) %>%
+  left_join(
+    train_target,
+    by = "ID"
+  ) %>%
+  mutate(
+    across(
+      ((contains("NWP2") | contains("NWP3")) &
+         (contains("_U") | contains("_V"))),
+      \(x) zoo::na.approx(x, maxgap = 2, na.rm = F))
+  ) %>%
+  pivot_longer(
+    cols = `NWP1_00h_D-2_U`:NWP4_12h_D_CLCT,
+    names_to = "name",
+    values_to = "value"
+  ) %>%
+  drop_na(value) %>%
+  separate(
+    col = name,
+    sep = "_",
+    into = c("met_model", "model_time", "forecast_time", "met_variable")
+  ) %>%
+  pivot_wider(
+    names_from = met_variable,
+    values_from = value
+  )
+
+test_data <- read_csv("dc_proj/X_test_v2.csv") %>%
+  mutate(
+    Time = dmy_hm(Time)
+  ) %>%
+  mutate(
+    across(
+      (
+        (contains("NWP2") | contains("NWP3")) &
+          (contains("_U") | contains("_V"))),
+      \(x) zoo::na.approx(x, maxgap = 2, na.rm = F))
+  ) %>%
+  pivot_longer(
+    cols = `NWP1_00h_D-2_U`:NWP4_12h_D_CLCT,
+    names_to = "name",
+    values_to = "value"
+  ) %>%
+  drop_na(value) %>%
+  separate(
+    col = name,
+    sep = "_",
+    into = c("met_model", "model_time", "forecast_time", "met_variable")
+  ) %>%
+  pivot_wider(
+    names_from = met_variable,
+    values_from = value
+  )
+
+
+saveRDS(train_data, file = "dc_proj/data_rds_files/x_train")
+saveRDS(test_data, file = "dc_proj/data_rds_files/x_test")
---
+++ explor_calibr_turbines.R
@@ -0,0 +1,157 @@
+rm(list = ls())
+
+library(plotly)
+library(tidyverse)
+
+x_test <- readRDS("dc_proj/data_rds_files/x_test")
+
+turbines <- read_delim(
+  "dc_proj/WindFarms_complementary_data.csv",
+  delim = ";",
+  skip = 1,
+  col_names = c(
+    "time",
+    "windfarm",
+    "windturbine",
+    "avgpower",
+    "cumulenergy",
+    "winddir",
+    "windspeed",
+    "nacdir",
+    "rotspeed")) %>%
+  mutate(time = dmy_hm(time))
+
+
+wf1 <- turbines %>%
+  filter(windfarm == "WF1")
+
+wf1 %>%
+  ggplot(data = .,
+         aes(x = windspeed, y = avgpower, col = windturbine)) +
+  facet_wrap(~windturbine) +
+  geom_point()
+
+turbines %>%
+  drop_na(windfarm, windturbine, avgpower) %>%
+  group_by(windfarm, windturbine) %>%
+  summarize(
+    c_zeros = sum(avgpower == 0, na.rm = T),
+    c_tot = n(),
+    frac = c_zeros/c_tot
+  ) %>%
+  arrange(desc(frac))
+
+turbines %>%
+  drop_na(windfarm, windturbine, avgpower) %>%
+  group_by(windfarm, hour = hour(time)) %>%
+  summarize(
+    c_zeros = sum(avgpower == 0, na.rm = T),
+    c_tot = n(),
+    frac = c_zeros/c_tot
+  ) %>%
+  ggplot(
+    data = .,
+    aes(x = hour, y = frac)) +
+  facet_wrap(~windfarm) +
+  geom_bar(position='stack', stat='identity')
+
+
+wf1 %>%
+  ggplot(
+    data = .,
+    aes(x = nacdir-winddir, y = cumulenergy, col = windturbine)) +
+  geom_point()
+
+turbines %>%
+  drop_na(nacdir, winddir, cumulenergy, windfarm) %>%
+  ggplot(
+    data = .,
+    aes(x = nacdir-winddir, y = cumulenergy, col = windturbine)) +
+  geom_point()
+
+wf1 %>%
+  drop_na(winddir, cumulenergy, windfarm) %>%
+  ggplot(
+    data = .,
+    aes(x = winddir, y = cumulenergy, col = windturbine)) +
+  geom_point()
+
+turbines %>%
+  filter(
+    !(windfarm %in% c("WF1", "WF6"))
+  ) %>%
+  plot_ly(
+    x = ~rotspeed,
+    y = ~abs(nacdir-winddir),
+    z = ~cumulenergy,
+    color = ~windfarm,
+    size = .2,
+    type = "scatter3d",
+    mode = "markers")
+
+turbines %>%
+  filter(
+    windfarm %in% c("WF1", "WF6")
+  ) %>%
+  plot_ly(
+    x = ~rotspeed,
+    y = ~abs(nacdir-winddir),
+    z = ~cumulenergy,
+    color = ~windfarm,
+    size = .2,
+    type = "scatter3d",
+    mode = "markers")
+  
+
+
+turbines %>%
+  drop_na(windfarm, rotspeed, cumulenergy) %>%
+  ggplot(
+    data = .,
+    aes(x = rotspeed, y = cumulenergy)
+  ) +
+  facet_wrap(~windfarm, scales = "free") + 
+  geom_point()
+
+
+turbines %>%
+  drop_na(windfarm, rotspeed, windspeed) %>%
+  ggplot(
+    data = .,
+    aes(x = windspeed, y = rotspeed)
+  ) +
+  facet_wrap(~windfarm, scales = "free") + 
+  geom_point()
+
+
+# if we could (accurately) predict windspeed AND rotspeed from
+#  the data we have, that would yield a (probably) very good prediction:
+turbines %>%
+  drop_na(windfarm, rotspeed, windspeed, cumulenergy) %>%
+  plot_ly(
+    x = ~windspeed,
+    y = ~rotspeed,
+    z = ~cumulenergy,
+    color = ~windfarm,
+    size = .2,
+    type = "scatter3d",
+    mode = "markers"
+  )
+
+turbines %>%
+  drop_na(windfarm, rotspeed, windspeed, cumulenergy) %>%
+  group_by(windfarm, time) %>%
+  summarize(
+    rotspeed = mean(rotspeed),
+    windspeed = mean(windspeed),
+    cumulenergy = sum(cumulenergy)
+  ) %>%
+  plot_ly(
+    x = ~windspeed,
+    y = ~rotspeed,
+    z = ~cumulenergy,
+    color = ~windfarm,
+    size = .2,
+    type = "scatter3d",
+    mode = "markers"
+  )
